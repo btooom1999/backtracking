18bcb9eb197bdd35debd31242fb9f87ffb0e81a4
btooom1999 <btooom1999@gmail.com>
2026-04-18 19:27:11 +0700

    finished non_decreasing_subsequences exercise

--- src/different_ways_to_add_parentheses.rs
+++
@@ -1,8 +0,0 @@
-fn diff_ways_to_compute(expression: String) -> Vec<i32> {
-    vec![]
-}
-
-pub fn main() {
-    let expression = "2-1-1".to_string();
-    println!("{:?}", diff_ways_to_compute(expression));
-}
---
+++ src/non_decreasing_subsequences.rs
@@ -0,0 +1,53 @@
+use std::collections::HashSet;
+
+fn dfs(
+    i: usize,
+    n: usize,
+    nums: &Vec<i32>,
+    dp: &mut Vec<Option<HashSet<Vec<i32>>>>,
+) -> HashSet<Vec<i32>> {
+    if i == n {
+        return HashSet::new();
+    }
+
+    if dp[i].is_some() {
+        return dp[i].clone().unwrap();
+    }
+
+    dfs(i+1, n, nums, dp);
+
+    let num = nums[i];
+    let mut vec = HashSet::from([vec![num]]);
+    for x in i+1..n {
+        if num > nums[x] {
+            continue;
+        }
+
+        for new_set in dfs(x, n, nums, dp) {
+            let mut set = vec![num];
+            set.extend(new_set);
+            vec.insert(set);
+        }
+    }
+
+    dp[i] = Some(vec.clone());
+    vec
+}
+
+fn find_subsequences(nums: Vec<i32>) -> Vec<Vec<i32>> {
+    let mut dp = vec![None; nums.len()];
+    dfs(0, nums.len(), &nums, &mut dp);
+
+    dp
+        .into_iter()
+        .flat_map(|v| v.unwrap().into_iter().collect::<Vec<_>>())
+        .collect::<HashSet<_>>()
+        .into_iter()
+        .filter(|v| v.len() > 1)
+        .collect::<Vec<_>>()
+}
+
+pub fn main() {
+    let nums = (1..=15).collect();
+    println!("{:?}", find_subsequences(nums));
+}
